4583a906c5df7b9acfbb484d561af11bb1c3f6dc
rent614 <rent31415@icloud.com>
2021-04-05 21:40:32 +0900

    商品の状態の記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,29 @@
 class Item < ApplicationRecord
+  has_many :comments
   belongs_to :user
+  has_one :purchase_history
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to_active_hash :category
-  belongs_to_active_hash :sales_status
-  belongs_to_active_hash :shipping_fee_status
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :scheduled_delivery
+  belongs_to_active_hash :description
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
+  belongs_to_active_hash :price
 
   with_options presence: true do
-    validates :title
-    validates :text
+    validates :items_name
+    validates :description
     validates :image
 
     with_options numericality: { other_than: 1 } do
-      validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id
+      validates :category
+      validates :description
+      validates :delivery_fee
+      validates :area
+      validates :day
     end
 
     with_options format: { with: /\A[0-9]+\z/ } do
