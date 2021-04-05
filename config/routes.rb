904f31cf7294c9733a37f12da697ee2fbf9ec821
rent614 <rent31415@icloud.com>
2021-04-05 17:57:43 +0900

    itemモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
+
+
