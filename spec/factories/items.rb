749ce1d09454de52120d0a61de5e67b515ef9691
rent614 <rent31415@icloud.com>
2021-04-07 17:05:09 +0900

    修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     items_name            {Faker::Name.initials}
     description           {Faker::Lorem.sentence}
-    category_id           {'2'}
-    status_id             {'3'}
-    delivery_fee_id       {'2'}
-    area_id               {'3'}
-    day_id                {'2'}
-    price                 {'1000'}  
+    category_id           {2}
+    status_id             {3}
+    delivery_fee_id       {2}
+    area_id               {3}
+    day_id                {2}
+    price                 {1000}  
     association :user
 
     after(:build) do |item|
