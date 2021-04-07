b90e0c2d096ff7f3e5cc438364b6715f34c1b324
rent614 <rent31415@icloud.com>
2021-04-07 12:51:44 +0900

    ログイン、非ログイン時のパスの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @items = Item.all
   end
@@ -10,13 +11,14 @@ class ItemsController < ApplicationController
   
   def create
     @item = Item.new(item_params)
-      if @item.valid?
-        @item.save
+      if @item.save
         redirect_to root_path
       else
         render :new
       end
   end
+
+  private
 
   def item_params
     params.require(:item).permit(:items_name, :description, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :price, :image
