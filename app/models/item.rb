d57c23ed9a7f1ac7ef09004669a82bfb41dcb076
rent614 <rent31415@icloud.com>
2021-04-07 22:36:32 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,16 @@
 class Item < ApplicationRecord
-  has_many :comments
+
+  # has_many :comments
   belongs_to :user
-  has_one :purchase_history
+  # has_one :purchase_history
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :delivery_fee
-  belongs_to_active_hash :area
-  belongs_to_active_hash :day
+  # belongs_to :category
+  # belongs_to :status
+  # belongs_to :delivery_fee
+  # belongs_to :area
+  # belongs_to :day
  
 
   with_options presence: true do
