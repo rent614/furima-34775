8f34bb1ccf4394b1e613d6606a00fda1202b874e
rent614 <rent31415@icloud.com>
2021-04-11 16:38:42 +0900

    orderモデル、コントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,36 +1,32 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy, :edit, :update]
-  before_action :move_to_index, except: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  
   def index
-    @items = Item.all.order(created_at: :desc)
+    @items = Item.all.order(created_at: "DESC")
+  end
+
+  def show
   end
 
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
-      if @item.save
+      if @item.valid?
+        @item.save
         redirect_to root_path
       else
         render :new
       end
   end
 
-  def show
-  end
-
-  def destroy
-      if current_user.id == @item.user_id 
-      @item.destroy
+  def edit
+    unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
-  end
-
-  def edit
-    
   end
 
   def update
@@ -41,22 +37,21 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
-  def set_item
-    @item = Item.find(params[:id])
-  end
-  
-
-  def item_params
-    params.require(:item).permit(:items_name, :description, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :price, :image
-    ).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+  def destroy
+    if user_signed_in? && current_user.id == @item.user_id
+      @item.destroy
       redirect_to root_path
     end
   end
-  
 
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+  params.require(:item).permit(:title, :text, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image
+  ).merge(user_id: current_user.id)
+  end
 end
