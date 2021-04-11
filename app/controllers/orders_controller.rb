4bcb0a49bc5227a6f42da4e19c93b7f96084c074
rent614 <rent31415@icloud.com>
2021-04-11 16:50:36 +0900

    コントローラーとviewの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  
+  def index
+    #if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
+      #@item_order = ItemOrder.all
+    #else
+      #redirect_to root_path
+    #end
+  end
+
+
+  def create
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      pay_item
+      @item_order.save
+      return redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
+  end
+
 end
