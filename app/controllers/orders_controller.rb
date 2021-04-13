bcab3aad9ea333ef0fef297557717c3511c7ea50
rent614 <rent31415@icloud.com>
2021-04-13 10:31:33 +0900

    card.jsの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
-  
-  def index
-    #if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
-      @item_order = ItemOrder.new
-    #else
-      #redirect_to root_path
-    #end
-  end
 
+  def index
+      @item_order = ItemOrder.new
+  end
 
   def create
     @item_order = ItemOrder.new(order_params)
     if @item_order.valid?
+      pay_item
       @item_order.save
       return redirect_to root_path
     else
@@ -28,7 +24,16 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:item_order).permit(:postcode, :area, :city, :block, :building, :phone_number ).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
+    params.require(:item_order).permit(:postcode, :area, :city, :block, :building, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
+    end
 
 end
