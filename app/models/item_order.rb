07041b8b46ca361c15ea4fd54ff2929825788944
rent614 <rent31415@icloud.com>
2021-04-14 15:59:58 +0900

    修正５

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -9,11 +9,9 @@ class ItemOrder
     validates :user_id
     validates :item_id
     validates :area_id, numericality: { other_than: 1 }
-    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :postcode, format: { with: /\A\d{3}[-]\d{4}\z/ }  
 
-    with_options format: { with: /\A\d{3}[-]\d{4}\z/ } do
-      validates :postcode
-    end
   end
 
   def save
