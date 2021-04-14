aa080c491d441b7e2b5148b9b0ceb78e24918c0e
rent614 <rent31415@icloud.com>
2021-04-14 16:15:19 +0900

    修正６

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user_id && @item.order == nil
+    if current_user.id != @item.user_id || @item.order != nil
       redirect_to root_path
     end
   end
